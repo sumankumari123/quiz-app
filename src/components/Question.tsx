33cb18a3fcd94d0299736f8ffe430722ffd24c28
unknown <vaibhavkumarsingh27012000@gmail.com>
2026-07-20 18:20:40 +0530

    selected option validition

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -3,12 +3,16 @@ import { option } from "./Option";
 import { useParams, useNavigate } from "react-router";
 
 const Question = () => {
-  const [count, setCount] = useState<number>(0);
+  const [selectedAns, setSelectedAns] = useState<string >();
   const { id } = useParams();
   const navigate = useNavigate();
 
   const questionId = Number(id);
-  // console.log(questionId);
+  console.log(selectedAns);
+
+  const handleSelectedOption = (selectedOps: string) => {
+    setSelectedAns(selectedOps);
+  };
 
   const handleNext = () => {
     // setCount(count + 1);
@@ -18,44 +22,48 @@ const Question = () => {
       navigate("/result");
     }
   };
-
+  console.log("option", option);
   return (
-    
-        <main className="max-w-2xl mx-auto">
-          {/* Title */}
-          <h1 className="text-5xl font-bold text-center mb-12">Quizapp</h1>
+    <main className="max-w-2xl mx-auto">
+      {/* Title */}
+      <h1 className="text-5xl font-bold text-center mb-12">Quizapp</h1>
 
-          {/* Question */}
-          <h2 className="text-3xl font-semibold text-center mb-8">
-            {option[count]?.question}
-          </h2>
+      {/* Question */}
+      <h2 className="text-3xl font-semibold text-center mb-8">
+        {option[questionId]?.question}
+      </h2>
 
-          {/* Options */}
-          <ul className="space-y-4">
-            {option[count]?.options.map((item) => (
-              <li
-                key={item}
-                className="bg-white border rounded-lg p-4 cursor-pointer hover:bg-indigo-600 hover:text-white transition duration-200"
-              >
-                {item}
-              </li>
-            ))}
-          </ul>
+      {/* Options */}
+      <ul className="space-y-4">
+        {option[questionId]?.options.map((item) => (
+          
+          <li
+  key={item}
+  onClick={() => handleSelectedOption(item)}
+  className={`${
+    Boolean(selectedAns)
+   && selectedAns === option[questionId].answer ?
+    "bg-green-400" : "bg-red-500"
+  } border rounded-lg p-4 cursor-pointer hover:bg-indigo-600 hover:text-white transition duration-200`}
+>
+  {item}
+</li>
+        ))} 
+      </ul>
 
-          {/* Next Button */}
-          <button
-            onClick={handleNext}
-            className="w-full mt-10 bg-cyan-100 hover:bg-cyan-200 rounded-xl py-3 text-lg font-semibold transition"
-          >
-            Next
-          </button>
+      {/* Next Button */}
+      <button
+        onClick={handleNext}
+        className="w-full mt-10 bg-cyan-100 hover:bg-cyan-200 rounded-xl py-3 text-lg font-semibold transition"
+      >
+        Next
+      </button>
 
-          {/* Question Counter */}
-          <p className="text-center mt-6 font-semibold">
-            Question {questionId} out of {option.length}
-          </p>
-        </main>
-     
+      {/* Question Counter */}
+      <p className="text-center mt-6 font-semibold">
+        Question {questionId + 1} out of {option.length}
+      </p>
+    </main>
   );
 };
 
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,19 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Result = () => {
   return (
-    <div>
-          <div className="min-h-screen bg-violet-300 flex items-center justify-center p-6">
-            <div className="w-full max-w-5xl bg-pink-300 rounded-2xl shadow-lg p-10">
-                <div className="flex flex-col items-center justify-center h-96">
-                  <h1 className="text-5xl font-bold mb-4">🎉 Thank You!</h1>
-                  <p className="text-xl">Quiz Completed Successfully.</p>
-                </div>
-             
-            </div>
-          </div>
+    <div className="flex flex-col items-center justify-center h-96">
+      <h1 className="text-5xl font-bold mb-4">🎉 Thank You!</h1>
+      <p className="text-xl">Quiz Completed Successfully.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
